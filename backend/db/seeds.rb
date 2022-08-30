fa236c8113de4098d484698a30cc15d584c4e4d5
Hiroto-Iizuka <oroxith@gmail.com>
2022-08-31 00:02:51 +0900

    feat: deviseを導入

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "管理者", 
+  email: "iizuka@example.com",
+  encrypted_password: "password",
+  admin: true)
+
+User.create!(name: "田中",
+             email: "tanaka@example.com",
+             encrypted_password: "password")
+
+User.create!(name: "佐藤",
+             email: "sato@example.com",
+             encrypted_password: "password")
+
+User.create!(name: "Watanabe",
+             email: "watanabe@example.com",
+             encrypted_password: "password")
+
+puts "初期データの投入に成功しました！"
